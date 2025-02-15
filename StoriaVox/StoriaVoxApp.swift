c90478c2b6c120dca19739d8066e23cd0e47aa1a
Dilan Rathnayaka <dilan.rathnayaka@infobric.com>
2025-02-15 14:45:36 +0200

    Initial Commit

---
+++ StoriaVox/StoriaVoxApp.swift
@@ -0,0 +1,17 @@
+//
+//  StoriaVoxApp.swift
+//  StoriaVox
+//
+//  Created by Dilan Anuruddha on 2025-02-15.
+//
+
+import SwiftUI
+
+@main
+struct StoriaVoxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StoriaVoxTests/StoriaVoxTests.swift
@@ -0,0 +1,17 @@
+//
+//  StoriaVoxTests.swift
+//  StoriaVoxTests
+//
+//  Created by Dilan Anuruddha on 2025-02-15.
+//
+
+import Testing
+@testable import StoriaVox
+
+struct StoriaVoxTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
