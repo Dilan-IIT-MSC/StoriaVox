63f68cc9fe97b5933a9de9a0fe08983f054bedd8
Dilan Rathnayaka <dilan.rathnayaka@infobric.com>
2025-03-10 21:49:40 +0200

    Add AppDelegate file.

---
+++ StoriaVox/AppDelegate/AppDelegate.swift
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.swift
+//  StoriaVox
+//
+//  Created by Dilan Anuruddha on 2025-03-10.
+//
+
+import UIKit
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        return true
+    }
+}
