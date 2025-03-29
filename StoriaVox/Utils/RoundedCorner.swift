ad73222825589317a9b293006923524e2dbb92e3
Dilan Rathnayaka <dilan.rathnayaka@infobric.com>
2025-03-29 21:39:32 +0200

    Adding corner radius extension.

---
+++ StoriaVox/Utils/RoundedCorner.swift
@@ -0,0 +1,25 @@
+//
+//  RoundedCorner.swift
+//  StoriaVox
+//
+//  Created by Dilan Anuruddha on 2025-03-29.
+//
+
+import Foundation
+import SwiftUI
+
+struct RoundedCorner: Shape {
+    var radius: CGFloat = .infinity
+    var corners: UIRectCorner = .allCorners
+
+    func path(in rect: CGRect) -> Path {
+        let path = UIBezierPath(roundedRect: rect, byRoundingCorners: corners, cornerRadii: CGSize(width: radius, height: radius))
+        return Path(path.cgPath)
+    }
+}
+
+extension View {
+    func cornerRadius(_ radius: CGFloat, corners: UIRectCorner) -> some View {
+        clipShape( RoundedCorner(radius: radius, corners: corners) )
+    }
+}
