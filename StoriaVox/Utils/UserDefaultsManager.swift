88223eaa598cb1c203ba1b26532e056b777fe94d
Dilan Rathnayaka <dilan.rathnayaka@infobric.com>
2025-04-04 12:09:49 +0300

    Implement user defaults manager

---
+++ StoriaVox/Utils/UserDefaultsManager.swift
@@ -0,0 +1,52 @@
+//
+//  UserDefaultsManager.swift
+//  StoriaVox
+//
+//  Created by Dilan Anuruddha on 2025-04-03.
+//
+
+import Foundation
+
+final class UserDefaultsManager {
+    static let shared = UserDefaultsManager()
+    private init() {}
+    
+    private let defaults = UserDefaults.standard
+
+    private enum Keys: String {
+        case isOnboardTourDone
+        case userPreferences
+    }
+    
+    var isOnboardTourDone: Bool {
+        get { defaults.bool(forKey: Keys.isOnboardTourDone.rawValue) }
+        set { defaults.set(newValue, forKey: Keys.isOnboardTourDone.rawValue) }
+    }
+
+    struct UserPreferences: Codable {
+        let prefersDarkMode: Bool
+        let languageCode: String
+    }
+
+    var userPreferences: UserPreferences? {
+        get {
+            guard let data = defaults.data(forKey: Keys.userPreferences.rawValue) else { return nil }
+            return try? JSONDecoder().decode(UserPreferences.self, from: data)
+        }
+        set {
+            if let newValue = newValue {
+                if let encoded = try? JSONEncoder().encode(newValue) {
+                    defaults.set(encoded, forKey: Keys.userPreferences.rawValue)
+                }
+            } else {
+                defaults.removeObject(forKey: Keys.userPreferences.rawValue)
+            }
+        }
+    }
+    
+    func removeAll() {
+        for key in [Keys.isOnboardTourDone, Keys.userPreferences] {
+            defaults.removeObject(forKey: key.rawValue)
+        }
+    }
+}
