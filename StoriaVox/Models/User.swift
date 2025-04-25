a34e28e708fbb672c12d721cc9be55075d1def86
Dilan Rathnayaka <dilan.rathnayaka@infobric.com>
2025-04-25 16:41:41 +0300

    Modify User & category Models.

--- StoriaVox/Models/User.swift
+++ StoriaVox/Models/User.swift
@@ -9,13 +9,15 @@ import Foundation
 
 struct User: Codable {
     let id: Int
+    let externalId: String
+    let email: String
     let firstName: String
     let lastName: String?
     let bday: String?
-    let status: Int
+    let bio: String?
     let profileImage: String?
+    let status: Int
+    let created: String?
+    let updated: String?
 }
 
-struct DeactivateResponse: Decodable {
-    let userId: Int
-}
